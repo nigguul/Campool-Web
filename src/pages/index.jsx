d178c6f40e710b3bc93cbdaba7a87b8828ddbed6
Victor Hugo <victor-hugo.gp@hotmail.com>
2021-11-16 21:52:54 -0300

    Major changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,110 @@
-import styles from "../styles/Home.module.css"
+import { useEffect, useRef, useState } from "react"
+import styles from "../styles/Home.module.scss"
+
+class Particles {
+    #particles;
+
+    constructor(box) {
+        this.#particles = [];
+        this.onUpdate = undefined;
+
+        this.h = box.getBoundingClientRect().height;
+        this.w = box.getBoundingClientRect().width;
+
+        this.gap = this.h * 0.15;
+
+        this.create();
+
+        this.updateID = requestAnimationFrame(this.update.bind(this));
+    }
+
+    create() {
+        let particle = {};
+
+        particle.size = Math.round(Math.random() * (this.w * 0.12 - this.w * 0.1) + this.w * 0.1);
+        particle.x = Math.round(Math.random() * (this.w - particle.size));
+        particle.offset = 0;
+        particle.opacity = 1;
+        
+        if (particle.x < this.w / 2) {
+            particle.type = 1;
+        } else {
+            particle.type = 0;
+        }
+
+        this.#particles.push(particle);
+    }
+
+    update() {
+        this.#particles.map((particle, index) => {
+            particle.offset += 1;
+            particle.opacity = 1 - (particle.offset / this.h);
+
+            if (particle.offset > this.h) this.#particles.splice(0, 1);
+            if (index == this.#particles.length - 1 && particle.offset >= this.gap) this.create();
+        })
+
+        if (this.onUpdate != undefined) this.onUpdate(this.#particles);
+
+        this.updateID = requestAnimationFrame(this.update.bind(this))
+    }
+
+    destroy() {
+        cancelAnimationFrame(this.updateID);
+    }
+}
 
 export default function Home() {
-    return (
-        <>
-            <head>
-                <title>CAMPOOL</title>
-                <link rel="favicon.ico" href="favicon.ico"/>
-            </head>
-            <div className={styles.home}>
-                <div className={styles.logo}>
-                    <img className={styles.logopart1} src="logopart1.svg" alt="tribo1"/>
-                    <img className={styles.logopart2} src="logopart2.svg" alt="tribo2"/>
-                    <img className={styles.logoimg} src="logoCampool.svg" alt="campool-logo"/>
-                </div>
+    let [effect, setEffect] = useState(undefined);
+    let [particles, setParticles] = useState([]);
 
-                <div className={styles.title}>
+    let effectBox = useRef();
+
+    useEffect(() => {
+        effect?.onUpdate = (p) => {
+            setParticles([...p]);
+        };
+
+        return () => {
+            effect?.onUpdate = undefined;
+        }
+    }, [effect]);
+
+    useEffect(() => {
+        if (effectBox != undefined) {
+            setEffect(new Particles(effectBox.current));
+        }
+    }, [effectBox]);
+
+    return (
+        <div className={styles.home}>
+            <div className={styles.logo}>
+                <div className={styles.effectBox} ref={effectBox}>
+                    {particles.map((particle, index) => {
+                        return (
+                            <span
+                                className={styles.particle}
+                                style={{
+                                    bottom: particle.offset,
+                                    left: particle.x,
+                                    width: particle.size * 0.75,
+                                    height: particle.size,
+                                    opacity: particle.opacity
+                                }}
+                                key={`particle${index}`}
+                                data-particle={particle.type}
+                            />
+                        )
+                    })}
+                </div>
+                <img className={styles.cloud} src="logopart1.svg" alt="tribo1"/>
+                <img className={styles.cloud} src="logopart2.svg" alt="tribo2"/>
+                <img className={styles.mainImg} src="logoCampool.svg" alt="campool-logo"/>
+                <span className={styles.neon}></span>
+            </div>
+
+            <div className={styles.title}>
+                <div className={styles.content}>
                     <h1>CAMPOOL</h1>
                     <span className={styles.spacer}></span>
                     <h4>Libere sua energia em nosso campeonato</h4>
@@ -24,7 +114,7 @@ export default function Home() {
                     </button>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/pages/principal/index.jsx
+++ src/pages/principal/index.jsx
@@ -1,4 +1,4 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.scss"
 
 export default function PagePrincipal() {
     return (
@@ -6,7 +6,28 @@ export default function PagePrincipal() {
             <head>
                 <title>CAMPOOL</title>  
             </head>
-            
+
+            <header className={styles.header}>
+
+                <div className={styles.leftInfo}>
+                    <img className={styles.mainImg} src="logoCampool.svg" alt="logo"/>
+                    <span className={styles.spacer}></span>
+                    <h1>CAMPOOL</h1>
+                </div>
+
+                <div className={styles.infos}>
+                    <img src="iconRank.svg" alt="iconrank" />
+                    <h2>RANKING MVP</h2>
+                    <img className={styles.imgRankTeam} src="iconRankTeam.svg" alt="iconrankteam" />
+                    <h2>RANKING TIMES</h2>
+                    <h2>SCRIMS</h2>
+                    <h2>FREE AGENTS</h2>
+                </div>
+
+                <div className={styles.User}>
+                    <img className={styles.exitImg} src="exit.svg" alt="sair" />
+                </div>
+            </header>
         </>
     )
 }
